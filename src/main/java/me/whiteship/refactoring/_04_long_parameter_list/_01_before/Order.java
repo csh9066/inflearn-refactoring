730d20c4c2742e250cf32cdad2d1fa57edb22d3c
seungho <50465901+csh9066@users.noreply.github.com>
2022-04-01 20:38:27 +0900

    매개 변수를 질의 함수로 바꾸기 실습

--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_01_before/Order.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_01_before/Order.java
@@ -1,5 +1,13 @@
 package me.whiteship.refactoring._04_long_parameter_list._01_before;
 
+/**
+ * 긴 매개변수 목록
+ * - 어떤 함수의 매개변수가 많을 수록 함수의 역할을 이해하기 어렵다
+ *  - 과연 그 함수가 한가지 일만 하고 있는 거인가?
+ *  - 불필요한 매개변수는 없는가?
+ *  - 하나의 자료구조로 뭉칠 수 있는가
+ */
+
 public class Order {
 
     private int quantity;
--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
@@ -1,5 +1,13 @@
 package me.whiteship.refactoring._04_long_parameter_list._14_replace_parameter_with_query;
 
+/**
+ * - 함수의 매개변수 목록은 함수의 다양성을 대변하며, 짧을수록 이해하기 좋다 길면 너무 많은 일을 하고 있을지도?
+ * - 어떠한 매개변수를 다른 매개변수로 알아 낼 수 있다면 중복 매개변수라 생각할 수 있다
+ * - 매겨변수의 값을 전달하는 것은 "함수의 호출하는 쪽" 즉 사용자 입장이다 사용자 입장에서 최대한 책임을 줄이고 내부에서 책임 지도록
+ * 해야한다
+ * - "임시 변수를 질의 변수로 바꾸기" 와 "함수 선언 바꾸기"를 통해 리팩토링 한다.
+ */
+
 public class Order {
 
     private int quantity;
@@ -12,12 +20,20 @@ public class Order {
     }
 
     public double finalPrice() {
-        double basePrice = this.quantity * this.itemPrice;
-        int discountLevel = this.quantity > 100 ? 2 : 1;
-        return this.discountedPrice(basePrice, discountLevel);
+        return this.discountedPrice();
     }
 
-    private double discountedPrice(double basePrice, int discountLevel) {
-        return discountLevel == 2 ? basePrice * 0.90 : basePrice * 0.95;
+    private int getDiscountLevel() {
+        int discountLevel = this.quantity > 100 ? 2 : 1;
+        return discountLevel;
+    }
+
+    private double getBasePrice() {
+        double basePrice = this.quantity * this.itemPrice;
+        return basePrice;
+    }
+
+    private double discountedPrice() {
+        return getDiscountLevel() == 2 ? getBasePrice() * 0.90 : getBasePrice() * 0.95;
     }
 }
